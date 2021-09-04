ecfbb5133cdbac3cc5a25ce64b6f1e563d89ec8d
DavoodSta <dchoopani@yahoo.com>
2021-09-04 12:57:08 +0430

    build project

--- app/src/main/java/com/star/customdialogexample/MainActivity.java
+++ app/src/main/java/com/star/customdialogexample/MainActivity.java
@@ -14,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+
+                //test
 //                CustomDialog.success(MainActivity.this, "", "this is test message", new OkButtonClickListener() {
 //                    @Override
 //                    public void onOKClick(DialogInterface dialog) {
